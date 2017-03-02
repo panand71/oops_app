3983720579379467833480f80c9f07075c8d3810
Premila Anand <panand71@github.com>
2017-03-01 20:11:38 -0800

    changes time delay to 30 seconds

---
+++ README.md
@@ -0,0 +1,14 @@
+# Opps App
+
+[The Oops App prevents ticket submission and allows a quick review of comments before submitting the ticket to Zendesk]
+
+### The following information is displayed:
+
+* Modal popup with comment text
+* Modal includes an abort button to cancel submission
+* Dismissing the modal or waiting 10 seconds will not prevent submission
+
+Please submit bug reports here in the Github Issues page. Pull requests are welcome.
+
+### Screenshot(s):
+![](http://i.imgur.com/9F78QXL.png)
---
+++ app.js
@@ -0,0 +1,121 @@
+(function () {
+
+  var DEFAULT_DELAY = 30,
+      cancelMessage,
+      cancelledMessage,
+      tickTimer,
+      stopped,
+      delay;
+
+  return {
+    events: {
+      'app.activated': 'init',
+      'ticket.save': 'save',
+      'ticket.submit.always': 'cleanTimer',
+      'click .cancel-ticket-submit': 'cancel',
+      'zd_ui_change .delay-settings-dropdown': 'updateSettings'
+    },
+
+    init: function () {
+      cancelMessage = helpers.fmt("<a tabindex='-1' onclick='$(\"#cancel-ticket-submit\").trigger(\"click\");'><strong>%@</strong></a>", this.I18n.t('cancelMessage'));
+      cancelledMessage = this.I18n.t('cancelledMessage');
+      delay = this.getDelay();
+      this.store({'ticketSubmissionUserDelay': delay});
+
+      this.switchTo('settings');
+      this.$('.delay-settings-dropdown').zdSelectMenu('setValue', delay);
+      if (!this.setting('allow_agents_choose_timeout')) {
+        this.hide();
+      }
+    },
+
+    updateSettings: function () {
+      var userDelay = parseInt(this.$('.delay-settings-dropdown').zdSelectMenu('value'), 10);
+      this.store({'ticketSubmissionUserDelay': userDelay});
+    },
+
+    cancel: function() {
+      stopped = true;
+    },
+
+    cleanTimer: function () {
+      clearInterval(tickTimer);
+    },
+
+    save: function () {
+      var uses_rich_text = this.comment().useRichText();
+      var comment_text;
+      if (uses_rich_text) {
+        var html_comment_text = this.comment().text();
+        comment_text = this.$(html_comment_text).text();
+      }
+      else {
+        comment_text = this.comment().text();
+      }
+
+      if (comment_text.length < 1) {
+        return true;
+      }
+
+      var tick = this.getDelay();
+      // bail out if delay set to off
+      if (tick <= 0) { return true; }
+
+      var self = this;
+      this.switchTo('modal');
+      this.show();
+      this.$('.my_modal').modal({
+        backdrop: false,
+        keyboard: false
+      });
+      this.$("p#message_to_save").append(comment_text);
+      this.$('.cancel-ticket-submit').click(function(){
+        self.cancel();
+        self.init();
+      });
+      this.$("#tick_timer").text(tick);
+
+      stopped = false;
+
+      return this.promise(function (done, fail) {
+        this.$('.save-ticket-submit').click(function(){
+          console.log('clicked save');
+          done();
+        });
+        var delay = self.getDelay();
+        tickTimer = setInterval(function () {
+          if (stopped) {
+            clearInterval(tickTimer);
+            self.$(".my_modal").hide();
+            if (self.setting('allow_agents_choose_timeout')) {
+              self.switchTo('settings');
+              self.$('.delay-settings-dropdown').zdSelectMenu('setValue', delay);
+            }
+            fail(cancelledMessage);
+          } else {
+            tick--;
+            self.$("#tick_timer").text(tick);
+            if (tick === 0) {
+              clearInterval(tickTimer);
+              self.$(".my_modal").hide();
+              if (self.setting('allow_agents_choose_timeout')) {
+                self.switchTo('settings');
+                self.$('.delay-settings-dropdown').zdSelectMenu('setValue', delay);
+              }
+              done();
+            }
+          }
+        }, 1000);
+      });
+    },
+
+    getDelay: function () {
+      var myDelay = this.store('ticketSubmissionUserDelay');
+      if (myDelay !== 0 && !myDelay) {
+        myDelay = DEFAULT_DELAY;
+      }
+
+      return myDelay;
+    }
+  };
+}());
